286d1299b135838ec8b31f647eba131d08ca1031
Christoph Blechschmitt <cblechschmitt@hiorg-server.de>
2018-02-10 16:54:02 +0100

    /bin/sh can't handle ssh-add <(echo

--- init.sh
+++ init.sh
@@ -7,10 +7,26 @@
 # SSH_PRIVATE_KEY
 # SSH_SERVER_HOSTKEY (i.e. copied from your local ~/.ssh/known_hosts)
 
+# on destination server:
+# > sudo $SSH_USER
+# > mkdir -p ~/.ssh
+# > nano ~/.ssh/authorized_keys
+# add corresponding public key (to this SSH_PRIVATE_KEY)
+# > chmod 600 ~/.ssh/authorized_keys
+# > chmod 700 ~/.ssh
+# > nano /etc/passwd
+# enable shell (i.e. /bin/bash ) for $SSH_USER
+# > nano /etc/ssh/sshd_config
+# add $SSH_USER to AllowUsers
+# > systemctl reload sshd
+
 echo ">>> Starting ssh-agent"
 # run ssh-agent in background
 eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
+echo "$SSH_PRIVATE_KEY" > privkey
+chmod 700 privkey
+ssh-add privkey
+rm privkey
 
 echo ">>> Add remote host key to known_hosts"
 mkdir -p ~/.ssh
