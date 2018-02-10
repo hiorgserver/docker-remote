094fe9b137bf74d48b4431c2a7e1e4f11ee9e3e1
Christoph Blechschmitt <cblechschmitt@hiorg-server.de>
2018-02-10 23:46:53 +0100

    more config on destination server

--- init.sh
+++ init.sh
@@ -7,28 +7,39 @@
 # SSH_PRIVATE_KEY
 # SSH_SERVER_HOSTKEY (i.e. copied from your local ~/.ssh/known_hosts)
 
-# on destination server:
+# on $SSH_HOST execute once:
+# > nano /etc/passwd
+# enable shell (i.e. /bin/bash ) for $SSH_USER
+# > passwd $SSH_USER
+# enter random password (will never be used)
+# > nano /etc/ssh/sshd_config
+# add $SSH_USER to AllowUsers
+# > systemctl reload sshd
+# > chown -R $SSH_USER. /home/of/ssh_user    # note the dot after username
 # > sudo $SSH_USER
 # > mkdir -p ~/.ssh
 # > nano ~/.ssh/authorized_keys
 # add corresponding public key (to this SSH_PRIVATE_KEY)
 # > chmod 600 ~/.ssh/authorized_keys
 # > chmod 700 ~/.ssh
-# > nano /etc/passwd
-# enable shell (i.e. /bin/bash ) for $SSH_USER
-# > nano /etc/ssh/sshd_config
-# add $SSH_USER to AllowUsers
-# > systemctl reload sshd
+# if you want to access another (git?) server by using this method,
+# you should connect NOW once manually, to accept its host fingerprint
+# > exit    # from sudo $SSH_USER
 
-echo ">>> Starting ssh-agent"
+
+if [ -z "$SSH_PORT" ]; then
+    SSH_PORT=22
+fi
+
+echo ">>> Starting ssh-agent, loading keys"
 # run ssh-agent in background
 eval $(ssh-agent -s)
+
 echo "$SSH_PRIVATE_KEY" > privkey
 chmod 700 privkey
 ssh-add privkey
 rm privkey
 
-echo ">>> Add remote host key to known_hosts"
 mkdir -p ~/.ssh
 echo "$SSH_SERVER_HOSTKEY" > ~/.ssh/known_hosts
 
